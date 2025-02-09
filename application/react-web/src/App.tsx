c74c8faa4f3c085f89d9b4965e8e4090c3fd7c28
ryanvflannery <ryanvflannery@gmail.com>
2025-02-09 01:03:29 -0800

    Adding react/vite/tailwindcss to frontend

---
+++ application/react-web/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-pink-200 min-h-screen flex items-center justify-center">
+      {/* Main Content */}
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-gray-900">
+          Welcome to Joystick Journal
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
