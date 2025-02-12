2f1bbeecff6afba2780a3c3b19d6b2385fa74ea1
ryanvflannery <ryanvflannery@gmail.com>
2025-02-11 20:08:26 -0800

    Added React Router for seamless transitions between pages

--- application/react-web/src/App.tsx
+++ application/react-web/src/App.tsx
@@ -1,23 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.tsx";
 import Footer from "./components/Footer.tsx";
 
 
 function App() {
   return (
-    <div className="bg-pink-200 min-h-screen">
+    <Router>
+    <div className="bg-pink-200 flex flex-col min-h-screen">
       {/* Navbar Component */}
       <Navbar />
 
       {/* Main Content */}
-      <div className="flex items-center justify-center min-h-screen">
-        <h1 className="mb-150 text-4xl font-bold text-gray-900">
+      <div className="flex-grow flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-900 -mt-80">
           Search
         </h1>
       </div>
       {/* Footer Sticks to the bottom of the page!*/}
       <Footer />
     </div>
-    
+    </Router>
   );
 }
 
